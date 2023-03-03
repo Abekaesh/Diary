d286cf0e563b6afb21220083ad87d875a2b30966
Abekaesh <abekaeshpa03@gmail.com>
2023-03-04 03:24:26 +0530

    Commit

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-import os
-
-import psycopg2
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import *
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+import psycopg2
 
-from helpers import apology, login_required
-
-# Configure application
 app = Flask(__name__)
-
+id = 0
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
@@ -33,201 +27,154 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-conn = psycopg2.connect(database="recipeholder", user="abe", password="123", port=5432, host="localhost")
+# Database connection
+conn = psycopg2.connect(database="diary", user="admin", password="admin", port=5432, host="localhost")
 db = conn.cursor()
+conn.rollback()
+conn.set_session(autocommit=True)
 
-
+app.secret_key='diary'
 @app.route("/")
-@login_required
-def index():
-    """Show your recipe dashboard"""
-    recipe_details = db.execute("""SELECT id, dish_name, updated_on FROM recipes WHERE user_id = {};""", session["user_id"])
-    return render_template("index.html", recipe_details=recipe_details)
+def home():
+    return render_template(
+        "index.html"
+    )
 
-@app.route("/add_recipe", methods=["GET", "POST"])
-@login_required
-def add_recipe():
+@app.route("/vision")
+def vision():
+    return render_template(
+        "about.html"
+    )
 
+@app.route("/dashboard",  methods=["GET", "POST"])
+@login_required
+def dashboard():
+
+    db.execute("SELECT * FROM tags;")
+    tags_present = db.fetchall()
     if request.method == "POST":
-        if not (dish_name := request.form.get("dish_name")):
-            return apology("must provide name of dish")
-        elif not (ingredients := request.form.get("ingredients")):
-            return apology("must provide ingredients")
-        elif not (recipe := request.form.get("recipe")):
-            return apology("recipe not entered")
-        rows = db.execute('SELECT * FROM recipes WHERE dish_name = ?;', dish_name)
+        if (date_entry := request.form.get('d_entry')):
+            tag = request.form.get('taglist')
+            if tag == 'all':
+                db.execute("SELECT * FROM diary_entry WHERE user_id = '{}' AND created_at = '{}' ORDER BY created_at DESC;".format(session['user_id'], date_entry))
+                entries = db.fetchall()
+                print(entries,1)
+                return render_template('dashboard.html', entries=entries, tags_present=tags_present)
+            else:
+                db.execute("SELECT * FROM diary_entry JOIN entry_tag ON diary_entry.entry_id = entry_tag.entry_id WHERE user_id = '{}' AND created_at = '{}' AND tag_id IN (SELECT tag_id FROM tags WHERE tag_name = '{}') ORDER BY created_at DESC;".format(session['user_id'], date_entry, tag))
+                entries = db.fetchall()
+                print(entries,2)
+                return render_template('dashboard.html', entries=entries, tags_present=tags_present)
+        else:
+            tag = request.form.get('taglist')
+            print(tag)
+            if tag!= None and tag!='all':
+                db.execute("SELECT * FROM diary_entry JOIN entry_tag ON diary_entry.entry_id = entry_tag.entry_id WHERE user_id = '{}' AND tag_id IN (SELECT tag_id FROM tags WHERE tag_name = '{}') ORDER BY created_at DESC;".format(session['user_id'], tag))
+                entries = db.fetchall()
+                print(entries,3)
+                return render_template('dashboard.html', entries=entries, tags_present=tags_present)
+            else:
+                db.execute("SELECT * FROM diary_entry WHERE user_id = '{}' ORDER BY created_at DESC;".format(session['user_id']))
+                entries = db.fetchall()
+                print(entries,4)
+                return render_template('dashboard.html', entries=entries, tags_present=tags_present)
+    
+    db.execute("SELECT * FROM diary_entry WHERE user_id = '{}' ORDER BY created_at DESC;".format(session['user_id']))
+    entries = db.fetchall()
+    return render_template(
+        "dashboard.html", entries=entries, tags_present=tags_present
+    )
 
+@app.route("/write", methods=["GET", "POST"])
+@login_required
+def write():
+    if request.method == "POST":
+        db.execute("SELECT * FROM diary_entry;")
+        r2 = db.fetchall()
+        entryid = len(r2) + 1
+        tags = request.form.get('tagname')
+        experience = request.form.get('experience')
+        tagnames = tags.split(',')
+        db.execute("INSERT INTO diary_entry(entry_id, user_id, entry_text) VALUES ('{}','{}','{}');".format(entryid, session['user_id'], experience))
+
+        for tag in tagnames:
+            db.execute("SELECT * FROM tags;")
+            r1 = db.fetchall()
+            
+            db.execute("SELECT * FROM tags WHERE tag_name = '{}';".format(tag))
+            rows = db.fetchone()
+            if rows is None:
+                tagid = len(r1) + 1
+                db.execute("INSERT INTO tags(tag_id, tag_name) VALUES('{}', '{}');".format(tagid, tag))
+                db.execute("INSERT INTO entry_tag(entry_id, tag_id) VALUES ('{}','{}');".format(entryid, tagid))
+            else:
+                db.execute("SELECT tag_id FROM tags WHERE tag_name = '{}';".format(tag))
+                r3 = db.fetchall()
+                tagid = r3[0][0]
+                db.execute("INSERT INTO entry_tag(entry_id, tag_id) VALUES ('{}','{}');".format(entryid, tagid))
+
+
+        flash("Your feelings are safe with me")
+        return redirect(url_for('dashboard'))
+    return render_template(
+        "write.html"
+    )
+
+@app.route("/signin", methods=["GET","POST"])
+def signin():
+    global id
+    if request.method=="POST":
+        name = request.form.get('username')
+        mailid = request.form.get('mailid')
+        dob = request.form.get('dob')
+        password = request.form.get('pass')
+        check_password = request.form.get('repass')
+        query = "SELECT * FROM users WHERE email = '{}';".format(mailid)
+        db.execute(query)
+        rows = db.fetchall()
         if len(rows) != 0:
-            return apology(f"The dish {dish_name} already exists. Try another name.")
+            flash(f"The email {mailid} already exists. Try another email.")
+            redirect(url_for('signin'))
+        elif password != check_password:
+            flash("Password not matching!")
+            redirect(url_for('signin'))
+        else:
+            db.execute("SELECT * FROM users;")
+            ids = db.fetchall()
+            id = len(ids) + 1
+            db.execute("INSERT INTO users (user_id, name, email, password, dob) VALUES ('{}', '{}', '{}', '{}', '{}');".format(id,name,mailid,password,dob) )
+            
+            session['user_id'] = id
+            flash("You are registered")
+            return redirect(url_for('dashboard'))
+    return render_template(
+        "signin.html"
+    )
 
-        db.execute("INSERT INTO recipes (user_id, dish_name, ingredients, recipe) VALUES (%s, %s, %s, %s);",
-                    session["user_id"], dish_name, ingredients, recipe)
-
-        flash("Recipe added successfully !!")
-
-        return redirect("/")
-    else:
-        return render_template("add_recipe.html")
-
-@app.route("/delete", methods=["GET", "POST"])
-@login_required
-def delete():
-    if request.method == "POST":
-        if not (dish_name := request.form.get("dish_name")):
-            return apology("MISSING DISH NAME")
-        if not (query := db.execute("SELECT * FROM recipes WHERE dish_name = %s;", dish_name)):
-            return apology("INVALID DISH NAME")
-        Id = query[0]["id"]
-        db.execute("DELETE FROM recipes WHERE dish_name = %s;", dish_name)
-        db.execute("UPDATE recipes SET id = id-1 WHERE id > %s;", Id)
-
-        flash("Recipe deleted successfully !!")
-        return redirect("/")
-    else:
-        return render_template("delete.html")
-
-
-@app.route("/recipe", methods = ["GET", "POST"])
-@login_required
-def recipe():
-    if request.method == "POST":
-        if not (dish_name := request.form.get("dish_name")):
-            return apology("MISSING DISH NAME")
-        if not (query := db.execute("SELECT * FROM recipes WHERE dish_name = %s AND user_id = %s;", dish_name, session["user_id"])):
-            return apology("INVALID DISH NAME")
-        flash("Recipe displayed below !!")
-        return render_template("recipe.html", dish_name=dish_name, ingredients=query[0]["ingredients"], recipe=query[0]["recipe"])
-    else:
-        return render_template("recipe.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET","POST"])
 def login():
-    """Log user in"""
-
-    # Forget any user_id
+    global id
     session.clear()
+    if request.method=="POST":
+        mailid = request.form.get('mailid')
+        password = request.form.get('pass')
+        query = "SELECT * FROM users WHERE email = '{}' AND password = '{}';".format(mailid, password)
+        db.execute(query)
+        rows = db.fetchall()
+        print(rows)
+        if len(rows) == 0:
+            flash("Wrong username or password!")
+            redirect(url_for('login'))
+        else:
+            session['user_id'] = rows[0][0]
+            flash("Welcome to your diary!")
+            return redirect(url_for('dashboard'))
+    return render_template(
+        "login.html"
+    )
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = %s", request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
+@app.route('/logout')
 def logout():
-    """Log user out"""
-
-    # Forget any user_id
     session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
-
-
-
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-    if request.method == "POST":
-
-        if not (username := request.form.get("username")):
-            return apology("MISSING USERNAME")
-
-        if not (password := request.form.get("password")):
-            return apology("MISSING PASSWORD")
-
-        if not (confirmation := request.form.get("confirmation")):
-            return apology("MISSING CONFIRMATION")
-
-        rows = db.execute(f"SELECT * FROM users WHERE username = {username};")
-
-        if len(rows) != 0:
-            return apology(f"The username {username} already exists. Try another username.")
-
-        if password != confirmation:
-            return apology("Password not Matched")
-
-        Id = db.execute(f"INSERT INTO users (username,hash) VALUES ({username},{generate_password_hash(password)});" )
-
-        session["user_id"] = Id
-
-        flash("Registered Successfully!!")
-
-        return redirect("/")
-
-    else:
-
-        return render_template("register.html")
-
-
-@app.route("/reset", methods=["GET", "POST"])
-@login_required
-def reset():
-    if request.method == "POST":
-        if not (password := request.form.get("password")):
-            return apology("MISSING OLD PASSWORD")
-
-        rows = db.execute("SELECT * FROM users WHERE id = %s;", session["user_id"])
-
-        if not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("INVALID PASSWORD")
-
-        if not (new_password := request.form.get("new_password")):
-            return apology("MISSING NEW PASSWORD")
-
-        if not (confirmation := request.form.get("confirmation")):
-            return apology("MISSING CONFIRMATION")
-
-        if new_password != confirmation:
-            return apology("PASSWORD NOT MATCH")
-
-        db.execute("UPDATE users set hash = %s WHERE id = %s;",
-                   generate_password_hash(new_password), session["user_id"])
-
-        flash("Password reset successful!")
-
-        return redirect("/")
-    else:
-        return render_template("reset_password.html")
-
-
-
-
-
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
-
-
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+    flash("See you later :)")
+    return redirect(url_for('home'))
